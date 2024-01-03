ef4be3f34e34277020acc7a62b281a83c0919e66
HaegyeongKim <Haegyeon.kim01@gmail.com>
2024-01-03 22:34:00 +0900

    feat: User 관련 코드 추가 및 수정

--- src/main/java/com/daycanvas/domain/user/UserController.java
+++ src/main/java/com/daycanvas/domain/user/UserController.java
@@ -5,9 +5,7 @@ import com.daycanvas.dto.user.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,20 +16,28 @@ public class UserController {
     private final ModelMapper modelMapper;
 
     @PostMapping("/join")
-    public ResponseEntity<Long> join (UserDto userDto) {
+    public ResponseEntity<Long> join (@RequestBody UserDto userDto) {
         User user = modelMapper.map(userDto, User.class);
-        Long userId = service.saveUser(user);
+        Long userId = service.save(user);
         return ResponseEntity.ok(userId);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Long> login (LoginDto loginDto) {
+    public ResponseEntity<Long> login (@RequestBody  LoginDto loginDto) {
         Long userId = service.login(loginDto.getEmail(), loginDto.getPassword());
-        if (userId != -1){
+
+        if (userId != null) {
             return ResponseEntity.ok(userId);
         }
-        return ResponseEntity.badRequest().body(-1L);
+        else {
+            // 로그인 실패 시에 대한 처리
+            return ResponseEntity.badRequest().build();
+        }
     }
 
-
+    @GetMapping("/delete")
+    public ResponseEntity<String> delete(@RequestParam Long userId) {
+        service.delete(userId);
+        return ResponseEntity.ok("User deleted successfully");
+    }
 }
