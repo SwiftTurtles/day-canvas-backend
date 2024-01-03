95d5291fa102f56cb405621d7c7bcc0ab92ad82e
HaegyeongKim <Haegyeon.kim01@gmail.com>
2024-01-04 03:11:17 +0900

    feat: PostService-update 메소드 추가

--- src/main/java/com/daycanvas/domain/post/PostService.java
+++ src/main/java/com/daycanvas/domain/post/PostService.java
@@ -15,8 +15,7 @@ public class PostService {
     private final PostRepository repository;
 
     public Long save(Post post) {
-        // @Todo image path -> AI Model과 연동
-        post.setImagePath("image_path");
+        post.setImagePath("image_path"); // @Todo image path -> AI Model 호출 <추후>
         return repository.save(post).getId();
     }
 
@@ -40,6 +39,22 @@ public class PostService {
     }
 
     public Long update(Post post) {
+        try {
+            Optional <Post> OptionalPost = repository.findById(post.getId());
+
+            if (OptionalPost.isPresent()) {
+                Post savedPost = OptionalPost.get();
+                savedPost.setContent(post.getContent());
+                savedPost.setImagePath("updated image Path");   // @Todo img 생성 ai 호출로 추푸 변경
+                repository.save(savedPost);
+                return savedPost.getId();
+            }
+        } catch (EmptyResultDataAccessException ex) {
+            throw new RuntimeException("Post not found for deletion.");
+        } catch (DataAccessException ex) {
+            // 그 외 데이터베이스 관련 예외 처리
+            throw new RuntimeException("Error finding exist post", ex);
+        }
         return null;
     }
 
